9c9792fc768eb9c3dc9f5678b08954e76a38d3ab
surim <yeom.surim@gmail.com>
2025-10-02 21:53:57 +0900

    ♻️ Refactor: yolov5s -> yolov5n

--- ros2_ws/src/example/example/self_driving/self_driving.launch.py
+++ ros2_ws/src/example/example/self_driving/self_driving.launch.py
@@ -42,7 +42,7 @@ def launch_setup(context):
         output='screen',
         parameters=[{'classes': ['cross_walk', 'light_green', 'light_red','light_yellow', 'parking', 'right', 'straight']},
             {"device": "cpu",
-            "model": "tfs_ver2",
+            "model": "bestn",
             "image_topic": "/ascamera/camera_publisher/rgb0/image",
             "camera_info_topic": "/camera/camera_info",
             "camera_info_file": f"{package_share_directory}/config/camera_info.yaml",
--- ros2_ws/src/example/example/self_driving/self_driving.py
+++ ros2_ws/src/example/example/self_driving/self_driving.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3
-# encoding: utf-8
-# @data:2023/03/28
-# @author:aiden
-# autonomous driving
-
 import os
 import cv2
 import math
@@ -29,7 +23,7 @@ from ros_robot_controller_msgs.msg import BuzzerState, SetPWMServoState, PWMServ
 
 
 class SelfDrivingNode(Node):
-    def __init__(self, name):
+    def __init__(self, name):   
         rclpy.init()
         super().__init__(name,
                          allow_undeclared_parameters=True,
@@ -40,8 +34,9 @@ class SelfDrivingNode(Node):
         self.pid = pid.PID(0.4, 0.0, 0.05)
         self.param_init()
 
+        self.debug = False   # debug 
         self.fps = fps.FPS()
-        self.image_queue = queue.Queue(maxsize=2)
+        self.image_queue = queue.Queue(maxsize=1)
         self.classes = ['cross_walk', 'light_green', 'light_red', 'light_yellow', 'parking', 'right', 'straight']
         self.display = True
         self.bridge = CvBridge()
@@ -74,12 +69,10 @@ class SelfDrivingNode(Node):
         self.timer.cancel()
         self.mecanum_pub.publish(Twist())
 
-        # YOLO는 only_line_follow == false일 때만 실행
         if not self.get_parameter('only_line_follow').value:
             self.send_request(self.start_yolov5_client, Trigger.Request())
         time.sleep(1)
 
-        # start 파라미터와 상관없이 항상 주행 시작
         self.display = True
         self.enter_srv_callback(Trigger.Request(), Trigger.Response())
         request = SetBool.Request()
@@ -112,7 +105,7 @@ class SelfDrivingNode(Node):
         self.crosswalk_length = 0.1 + 0.3
         self.start_slow_down = False
         self.normal_speed = 0.3
-        self.slow_down_speed = 0.15 # original 0.1
+        self.slow_down_speed = 0.14 # original 0.1
         self.traffic_signs_status = None
         self.red_loss_count = 0
         self.object_sub = None
@@ -120,7 +113,12 @@ class SelfDrivingNode(Node):
         self.objects_info = []
         self.stuck_count = 0
         self.after_turn = False
-
+ 
+        # self.crosswalk_stop = False
+        self.crosswalk_detected = False
+        self.crosswalk_stop_start_time = None
+        self.crosswalk_cooldown_time = None  
+        self.crosswalk_cooldown_duration = 10.0  
     def get_node_state(self, request, response):
         response.success = True
         return response
@@ -131,13 +129,12 @@ class SelfDrivingNode(Node):
             if future.done() and future.result():
                 return future.result()
 
-    def enter_srv_callback(self, request, response):
+    def enter_srv_callback(self, request, response):    
         self.get_logger().info('\033[1;32m%s\033[0m' % "self driving enter")
         with self.lock:
             self.start = False
             self.create_subscription(Image, '/ascamera/camera_publisher/rgb0/image', self.image_callback, 1)
 
-            # YOLO 구독은 only_line_follow == false일 때만
             if not self.get_parameter('only_line_follow').value:
                 self.create_subscription(ObjectsInfo, '/yolov5_ros2/object_detect', self.get_object_callback, 1)
 
@@ -147,7 +144,7 @@ class SelfDrivingNode(Node):
         response.message = "enter"
         return response
 
-    def exit_srv_callback(self, request, response):
+    def exit_srv_callback(self, request, response):     
         self.get_logger().info('\033[1;32m%s\033[0m' % "self driving exit")
         with self.lock:
             self.mecanum_pub.publish(Twist())
@@ -156,10 +153,10 @@ class SelfDrivingNode(Node):
         response.message = "exit"
         return response
 
-    def set_running_srv_callback(self, request, response):
+    def set_running_srv_callback(self, request, response):  
         self.get_logger().info('\033[1;32m%s\033[0m' % "set_running")
         with self.lock:
-            self.start = request.data
+            self.start = request.data   
             if not self.start:
                 self.mecanum_pub.publish(Twist())
         response.success = True
@@ -169,17 +166,17 @@ class SelfDrivingNode(Node):
     def shutdown(self, signum, frame):
         self.is_running = False
 
-    def image_callback(self, ros_image):
+    def image_callback(self, ros_image):   
         cv_image = self.bridge.imgmsg_to_cv2(ros_image, "bgr8")
         rgb_image = np.array(cv_image, dtype=np.uint8)
-        if self.image_queue.full():
+        if self.image_queue.full():           
             self.image_queue.get()
         self.image_queue.put(rgb_image)
 
     def main(self):
         while self.is_running:
             time_start = time.time()
-            try:
+            try:    
                 image = self.image_queue.get(block=True, timeout=1)
             except queue.Empty:
                 if not self.is_running:
@@ -189,45 +186,68 @@ class SelfDrivingNode(Node):
 
             result_image = image.copy()
             if self.start:
-                binary_image = self.lane_detect.get_binary(image)
-                cv2.imshow("binary", binary_image)
-                cv2.waitKey(1)
+                binary_image = self.lane_detect.get_binary(image)              
+                #cv2.imshow("binary", binary_image)
+                #cv2.waitKey(1)
                 twist = Twist()
 
                 # line following
                 result_image, lane_angle, lane_x = self.lane_detect(binary_image, image.copy())
-                self.get_logger().info(f'\033[1;32mlane_x: {lane_x}\033[0m')
-                self.get_logger().info(f'\033[1;32mlane_angle: {lane_angle}\033[0m')
+                # self.get_logger().info(f'\033[1;32mlane_x: {lane_x}\033[0m') # 寃�異쒕맂 李⑥꽑 以묒떖??x醫뚰몴
+                # self.get_logger().info(f'\033[1;32mlane_angle: {lane_angle}\033[0m')    # 李⑥꽑 媛곷룄 (吏꾪뻾 諛⑺뼢)
 
+                if self.crosswalk_detected:
+                    if self.crosswalk_stop_start_time is None:
+                        self.crosswalk_stop_start_time = time.time()
+                    elapsed = time.time() - self.crosswalk_stop_start_time
+                    if elapsed < 3.0:
+                        self.crosswalk_stop = True
+                        # self.get_logger().info(f'\033[1;32mSTOP\033[0m')
+                    else:
+                        self.crosswalk_stop = False
+                        self.crosswalk_detected = False
+                        self.crosswalk_stop_start_time = None
+                        self.crosswalk_cooldown_time = time.time()
+                        self.get_logger().info(f'\033[1;32mSTART IGNORE\033[0m')
+                else:
+                    self.crosswalk_stop = False
+                    self.crosswalk_stop_start_time = None
+
+                if self.crosswalk_stop:  
+                    twist.linear.x = 0.0
+                    twist.angular.z = 0.0
+                    self.mecanum_pub.publish(twist)
+                    continue  
                 if lane_x >= 0 and not self.stop:
-                    self.get_logger().info(f'\033[1;32mStart Tracking\033[0m')
+                    # self.get_logger().info(f'\033[1;32mStart Tracking\033[0m')
                     if lane_x > 270:
                         self.count_turn += 1
                         if self.count_turn > 5:
                             self.get_logger().info(f'\033[1;32mTurn\033[0m')
                             self.count_turn = 0
                             twist.linear.x = self.slow_down_speed
-                            twist.angular.z = -0.3
+                            twist.angular.z = -0.6
                             self.after_turn = True
                     else:
                         twist.linear.x = self.normal_speed
                         self.count_turn = 0
-                        if self.after_turn:
-                            self.pid.SetPoint = 130
-                            self.pid.update(lane_x)
-                            twist.angular.z = common.set_range(self.pid.output, -0.1, 0.1)
-                            self.after_turn = False
-                    self.mecanum_pub.publish(twist)
+                        if lane_x >= 0 and not self.stop:
+                            # 보정값 (좌표 차이)
+                            error = lane_x - 130  # lane_x가 130보다 크면 오른쪽 → 좌회전 필요
+                            k = 0.002             # 비례 상수 (튜닝 필요)
+                            twist.linear.x = self.normal_speed
+                            twist.angular.z = -k * error  # error > 0 → 왼쪽으로 회전 (부호 맞게 튜닝)
+
+                            self.mecanum_pub.publish(twist)
                 else:
                     self.pid.clear()
                     self.stuck_count += 1
                     if self.stuck_count > 5:
                         twist.linear.x = self.slow_down_speed
-                        twist.angular.z = -0.3
+                        twist.angular.z = -0.6
                         self.after_turn = True
                     self.mecanum_pub.publish(twist)
 
-                # YOLO 시각화는 only_line_follow == false일 때만
                 if not self.get_parameter('only_line_follow').value and self.objects_info:
                     for i in self.objects_info:
                         box = i.box
@@ -257,20 +277,57 @@ class SelfDrivingNode(Node):
         self.mecanum_pub.publish(Twist())
         rclpy.shutdown()
 
-    def get_object_callback(self, msg):
+    def get_object_callback(self, msg):  
         self.objects_info = msg.objects
-        if self.objects_info == []:
+        self.crosswalk_detected = False
+        self.crosswalk_distance = 0
+
+        with self.lock:
+            if self.crosswalk_cooldown_time:
+                if time.time() - self.crosswalk_cooldown_time < self.crosswalk_cooldown_duration:
+                    return
+                else:
+                    self.crosswalk_cooldown_time = None 
+                    self.get_logger().info(f'\033[1;32mEND IGNORE\033[0m')
+
+        if not self.objects_info:
             self.traffic_signs_status = None
-            self.crosswalk_distance = 0
-        else:
-            min_distance = 0
-            for i in self.objects_info:
-                class_name = i.class_name
-                center = (int((i.box[0] + i.box[2])/2), int((i.box[1] + i.box[3])/2))
-                if class_name == 'cross_walk':
-                    if center[1] > min_distance:
-                        min_distance = center[1]
-            self.crosswalk_distance = min_distance
+            return
+
+        max_score = 0
+        best_center_y = 0
+        frame_width = 640
+
+        for obj in self.objects_info:
+            if obj.class_name == 'cross_walk':
+                score = obj.score
+                x1, y1, x2, y2 = obj.box
+                width = x2 - x1
+                height = y2 - y1
+                aspect_ratio = width / (height + 1e-5)
+                center_y = int((y1 + y2) / 2)
+
+                self.get_logger().info(f"[CW?] s={score:.2f} w={width} ar={aspect_ratio:.2f} y2={y2}")
+
+                
+
+                if score > 0.7 and width > frame_width * 0.55 and aspect_ratio > 2.0:
+                    if score > max_score:
+                        max_score = score
+                        self.crosswalk_detected = True
+                        best_center_y = center_y
+                        self.get_logger().info(f'\033[1;32mScore: {score} Width: {width} ar={aspect_ratio:.2f} best_y={best_center_y}\033[0m')
+
+                        if best_center_y > 360:
+                            self.get_logger().info("횡단보도 앞 도착 → 정지!")
+                            twist = Twist()
+                            twist.linear.x = 0.0
+                            twist.angular.z = 0.0
+                            self.mecanum_pub.publish(twist)
+
+        self.crosswalk_distance = best_center_y if self.crosswalk_detected else 0
+
+
 
 
 def main():
